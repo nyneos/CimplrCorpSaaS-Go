85f4e07dcae3c99c4b6687024b85deb2efb41ab9
Hardik-nyneos <hardik.mishra@nyneos.com>
2025-08-26 01:47:03 +0530

    Fix port configuration for Master Service

--- api/master/master.go
+++ api/master/master.go
@@ -3,7 +3,6 @@ package master
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
 func StartMasterService() {
@@ -12,13 +11,7 @@ func StartMasterService() {
 		w.Write([]byte("Hello from Master Service"))
 	})
 	log.Println("Master Service started on :2143")
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "2143" // fallback for local dev
-	}
-	log.Printf("Master Service started on :%s", port)
-	err := http.ListenAndServe(":"+port, mux)
-
+	err := http.ListenAndServe(":2143", mux)
 	if err != nil {
 		log.Fatalf("Master Service failed: %v", err)
 	}
