d2fbc4cf681cd2f313d8f9b7d13c79216a486fd3
ka-nyneos <kanav.arora@nyneos.com>
2025-08-27 23:37:50 +0530

    Update gateway.go

--- api/gateway.go
+++ api/gateway.go
@@ -147,19 +147,6 @@ func (rw *responseWriter) Write(b []byte) (int, error) {
 	return rw.ResponseWriter.Write(b)
 }
 
-func withCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 // StartGateway starts the API gateway server
 func StartGateway() {
 	mux := http.NewServeMux()
