e7cd5b59b628c1c53ff8ea57c84e98b8ffaa3523
Hardik-nyneos <hardik.mishra@nyneos.com>
2025-08-29 03:47:04 +0530

    Entity

--- api/master/master.go
+++ api/master/master.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+
+	"CimplrCorpSaas/api"
+	allMaster "CimplrCorpSaas/api/master/allMasters"
 )
 
 func StartMasterService(db *sql.DB) {
@@ -11,6 +14,16 @@ func StartMasterService(db *sql.DB) {
 	mux.HandleFunc("/master/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from Master Service"))
 	})
+
+	mux.Handle("/master/entity/bulk-create-sync", api.BusinessUnitMiddleware(db)(allMaster.CreateAndSyncEntities(db)))
+	mux.Handle("/master/entity/hierarchy", api.BusinessUnitMiddleware(db)(allMaster.GetEntityHierarchy(db)))
+	mux.Handle("/master/entity/delete", api.BusinessUnitMiddleware(db)(allMaster.DeleteEntity(db)))
+	mux.Handle("/master/entity/findParentAtLevel", api.BusinessUnitMiddleware(db)(allMaster.FindParentAtLevel(db)))
+	mux.Handle("/master/entity/approve", api.BusinessUnitMiddleware(db)(allMaster.ApproveEntity(db)))
+	mux.Handle("/master/entity/reject-bulk", api.BusinessUnitMiddleware(db)(allMaster.RejectEntitiesBulk(db)))
+	mux.Handle("/master/entity/update", api.BusinessUnitMiddleware(db)(allMaster.UpdateEntity(db)))
+	mux.Handle("/master/entity/all-names", api.BusinessUnitMiddleware(db)(allMaster.GetAllEntityNames(db)))
+	mux.Handle("/master/entity/render-vars-hierarchical", api.BusinessUnitMiddleware(db)(allMaster.GetRenderVarsHierarchical(db)))
 	log.Println("Master Service started on :2143")
 	err := http.ListenAndServe(":2143", mux)
 	if err != nil {
